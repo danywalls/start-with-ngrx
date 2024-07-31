b1c12a76d2e3f1c95e8df754ecac1de0867e64e5
Dany Paredes <danywalls4@gmail.com>
2024-07-31 09:05:50 +0200

    feat(places): add places page and api actions
    define the actions to trigger and listen in the reducer and effect for place feature.

---
+++ src/app/pages/places/state/places.actions.ts
@@ -0,0 +1,26 @@
+import { createActionGroup, emptyProps, props } from '@ngrx/store';
+import { Place } from '../../../entities/place.model';
+
+export const PlacesPageActions = createActionGroup({
+  source: 'Places',
+  events: {
+    'Load Places': emptyProps(),
+    'Add Place': props<{ place: Place }>,
+    'Update Place': props<{ place: Place }>,
+    'Delete Place': props<{ id: string }>,
+  },
+});
+
+export const PlacesApiActions = createActionGroup({
+  source: 'PlaceAPI',
+  events: {
+    'Load Success': props<{ places: Array<Place> }>,
+    'Load Fail': props<{ message: string }>,
+    'Add Success': props<{ place: Place }>,
+    'Add Failure': props<{ message: string }>,
+    'Update Success': props<{ place: Place }>,
+    'Update Failure': props<{ message: string }>,
+    'Delete Success': props<{ id: string }>,
+    'Delete Failure': props<{ message: string }>,
+  },
+});
