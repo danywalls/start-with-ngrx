f62ed1c1c55fc3abd85e1b9e3d1fb0bdb0bef5fa
Dany Paredes <danywalls4@gmail.com>
2024-07-31 11:58:53 +0200

    feat(places): add places effect handle requests

---
+++ src/app/pages/places/state/places.effects.ts
@@ -0,0 +1,73 @@
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { inject } from '@angular/core';
+import { PlacesService } from '../../../services/places.service';
+import { PlacesApiActions, PlacesPageActions } from './places.actions';
+import { catchError, concatMap, exhaustMap, map, mergeMap, of } from 'rxjs';
+
+export const loadPlacesEffect$ = createEffect(
+  (actions$ = inject(Actions), placesService = inject(PlacesService)) => {
+    return actions$.pipe(
+      ofType(PlacesPageActions.loadPlaces),
+      exhaustMap(() =>
+        placesService.getAll().pipe(
+          map((places) => PlacesApiActions.loadSuccess({ places })),
+          catchError((error) =>
+            of(PlacesApiActions.loadFailure({ message: error })),
+          ),
+        ),
+      ),
+    );
+  },
+);
+
+export const updatePlaceEffect$ = createEffect(
+  (actions$ = inject(Actions), placesService = inject(PlacesService)) => {
+    return actions$.pipe(
+      ofType(PlacesPageActions.addPlace),
+      concatMap(({ place }) =>
+        placesService.update(place).pipe(
+          map((apiPlace) =>
+            PlacesApiActions.updateSuccess({ place: apiPlace }),
+          ),
+          catchError((error) =>
+            of(PlacesApiActions.updateFailure({ message: error })),
+          ),
+        ),
+      ),
+    );
+  },
+);
+
+export const addPlaceEffect$ = createEffect(
+  (actions$ = inject(Actions), placesService = inject(PlacesService)) => {
+    return actions$.pipe(
+      ofType(PlacesPageActions.addPlace),
+      mergeMap(({ place }) =>
+        placesService.add(place).pipe(
+          map((apiPlace) => PlacesApiActions.addSuccess({ place: apiPlace })),
+          catchError((error) =>
+            of(PlacesApiActions.addFailure({ message: error })),
+          ),
+        ),
+      ),
+    );
+  },
+);
+
+export const deletePlaceEffect$ = createEffect(
+  (actions$ = inject(Actions), placesService = inject(PlacesService)) => {
+    return actions$.pipe(
+      ofType(PlacesPageActions.deletePlace),
+      mergeMap(({ id }) =>
+        placesService.delete(id).pipe(
+          map((id_response) =>
+            PlacesApiActions.deleteSuccess({ id: id_response }),
+          ),
+          catchError((error) =>
+            of(PlacesApiActions.deleteFailure({ message: error })),
+          ),
+        ),
+      ),
+    );
+  },
+);
