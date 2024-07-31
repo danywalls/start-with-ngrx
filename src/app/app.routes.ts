39cbc5ea457d68e58f69ae89e5829e61fbb6545d
Dany Paredes <danywalls4@gmail.com>
2024-07-31 07:57:29 +0200

    feat(place feature): add the place component and register routes and navigation.
    add the place component and register in the routing system and navigation.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,23 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { PlacesComponent } from './pages/places/places.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: HomeComponent
-    },
-    {
-        path: 'about',
-        component: AboutComponent
-    },
-    {
-        path: '**',
-        redirectTo: ''
-    }
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'places',
+    component: PlacesComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
---
+++ src/app/pages/places/places.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-places',
+  standalone: true,
+  imports: [],
+  templateUrl: './places.component.html',
+  styleUrl: './places.component.scss',
+})
+export class PlacesComponent {}
