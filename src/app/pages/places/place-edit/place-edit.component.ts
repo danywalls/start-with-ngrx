fdc621181b3fa3ec04033b7231d3d2f2292a2adc
Dany Paredes <danywalls4@gmail.com>
2024-08-03 23:29:00 +0200

    fix(place card): remove not used parameter

--- src/app/pages/places/place-edit/place-edit.component.ts
+++ src/app/pages/places/place-edit/place-edit.component.ts
@@ -1,7 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import PlacesSelectors from '../state/places.selectors';
 import { AsyncPipe, JsonPipe } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { PlacesPageActions } from '../state/places.actions';
 
 @Component({
   selector: 'app-place-edit',
@@ -10,7 +12,15 @@ import { AsyncPipe, JsonPipe } from '@angular/common';
   templateUrl: './place-edit.component.html',
   styleUrl: './place-edit.component.scss',
 })
-export class PlaceEditComponent {
+export class PlaceEditComponent implements OnInit {
   store = inject(Store);
   place$ = this.store.select(PlacesSelectors.selectPlaceSelected);
+  activatedRoute = inject(ActivatedRoute);
+
+  ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.params['id'];
+    if (id) {
+      this.store.dispatch(PlacesPageActions.editPlace({ id }));
+    }
+  }
 }
